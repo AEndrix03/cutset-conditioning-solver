85abef43bf9bf200cda7ac8f61e7f6585b447dc3
AEndrix <redegalli.andrea03@gmail.com>
2026-07-06 02:41:51 +0200

    feat: CSP base types & alg

--- include/csp.hpp
+++ include/csp.hpp
@@ -1,32 +1,185 @@
+// Questa classe contiene i tipi base per un CSP, con tipi come alias e concetti vari
+
+#pragma once
+
 #include <vector>
 #include <string>
+#include <functional>
+#include <memory>
 
-// alias per descrivere meglio i problemi
-using Var = int;
-using Value = int;
-using Domain = std::vector<int>;
-using Domains = std::vector<Domain>;
-using Assignment = std::vector<int>; // con -1 = non assegnato
+using Var = int; // Alias per rappresentare una variabile
+using Value = int; // Alias per rappresentare un valore assunto da una variabile
+using Domain = std::vector<Value>; // Alias che rappresenta il dominio di una variabile, ossia la lista dei valori che la var può assumere
+using Domains = std::vector<Domain>; // Domini
+using Assignment = std::vector<Value>; // Rappresenta un'assegnazione parziale o completa
 
-struct BinaryConstraint {
-	Var a, b;
-	std::function<bool(Value, Value)> ok;
-	std::string name;
-}
-
-struct CSP {
-	std::string name;
-	int nvars;
-	Domains domains;
-	std::vector<BinaryConstraint> constraints>;
-}
-
+/**
+ * Statistiche Solver, misura ciò che succede durante la risoluzione del CSP con il Solver
+ */
 struct SolverStats {
-	long long nodes = 0;
-	long long constraints_check = 0;
-	long long cutset_assignments = 0;
-	int cutset_size = 0;
-	double milliseconds = 0.0;
-	bool solved = false;
-	bool timed_out = false;
-}
+    long long nodes = 0;
+    long long constraint_checks = 0;
+    long long cutset_assignments = 0;
+    int cutset_size = 0;
+    double milliseconds = 0.0;
+    bool solved = false;
+    bool timed_out = false;
+};
+
+/*
+ * Concetti teorici da ricordare:
+ * - Assegnamento:
+ *      - Parziale: valori assegnati a una variabile, ma ancora non tutti;
+ *      - Completo: valori assegnati a una variabile, tutti.
+ * - Consistenza:
+ *      - Un'assegnamento è consistente se per ogni x assegnato si rispettano i vincoli (constraints)
+ *
+ * Un assegnamento è detto Soluzione se Completo e Consistente!
+ */
+
+/**
+ * Rappresenta un qualsiasi vincolo (constraint) binario, ossia che relaziona su due variabili.
+ * Implementazione generalizzata per poter costruire vincoli specifici con dei nomi.
+ *
+ * Un vincolo binario collega due variabili del CSP, quindi richiede le due variabili
+ */
+class BinaryConstraint {
+public:
+    BinaryConstraint(Var first, Var second, std::string name);
+
+    virtual ~BinaryConstraint() = default;
+
+    Var first() const;
+    Var second() const;
+    const std::string& name() const;
+
+    /**
+     * @param x Variabile
+     * @return Ritorna true se la variabile passata costituisce il vincolo, false altrimenti
+     */
+    bool involves(Var x) const;
+
+    /**
+     * @param x Variabile X
+     * @param y Variabile Y
+     * @return Ritorna true se il vincolo collega X e Y, false altrimenti
+     */
+    bool connects(Var x, Var y) const;
+
+    /**
+     * @param x Variabile
+     * @return Data una delle due variabili, ritorna l'altra. Utile al backtracing
+     */
+    Var other(Var x) const;
+
+
+    /**
+     * @param first_value Valore assegnato alla prima variabile
+     * @param second_value Valore assegnato alla seconda variabile
+     * @return Se soddisfa il vincolo, true
+     */
+    virtual bool is_satisfied(Value first_value, Value second_value) const = 0;
+
+private:
+    Var first_;
+    Var second_;
+    std::string name_;
+};
+
+/**
+ * Sottoclasse di vincolo, su uguaglianze
+ */
+class NotEqualConstraint : public BinaryConstraint {
+public:
+    NotEqualConstraint(Var first, Var second, std::string name = "");
+
+    bool is_satisfied(Value a, Value b) const override;
+};
+
+/**
+ * Sottoclasse di vincolo, su comparazioni
+ */
+class LessThanConstraint : public BinaryConstraint {
+public:
+    LessThanConstraint(Var first, Var second, std::string name = "");
+
+    bool is_satisfied(Value a, Value b) const override;
+};
+
+/**
+ * Sottoclasse di vincolo, su funzioni predicate qualsiasi (default)
+ */
+class PredicateConstraint : public BinaryConstraint {
+public:
+    PredicateConstraint(Var first, Var second, std::function<bool(Value, Value)> predicate, std::string name = "");
+
+    bool is_satisfied(Value a, Value b) const override;
+
+private:
+    std::function<bool(Value, Value)> predicate_;
+};
+
+
+/**
+* Classe principale del progetto, rappresenta un CSP, ossia un
+ * Constraint Satisfaction Problem
+*/
+class CSP {
+public:
+    CSP(std::string name, Domains domains);
+
+    const std::string& name() const; // Nome del problema
+    int nvars() const; // Numero di variabili
+
+    const Domains& domains() const; // Domini delle variabili
+    const std::vector<std::unique_ptr<BinaryConstraint>>& constraints() const; // Vincoli del problema
+
+    Assignment empty_assignment() const; // Crea un assegnamento vuoto, CONVENZIONE DI PROGETTO: Valore -1 se non assegnato
+
+    void add_constraint(std::unique_ptr<BinaryConstraint> constraint); // Aggiunge un vincolo al CSP
+
+    /**
+     * Controlla tutti i vincoli tra due variabili assegnate, serve perchè tra la stessa coppia possono esistere più
+     * vincoli, anche se il primal graph mostrerà solo un arco.
+     * @param x Variabile X
+     * @param vx Valore associato alla Variabile X
+     * @param y Variabile Y
+     * @param vy Valore associato alla Variabile Y
+     * @param stats Statistica Solver
+     */
+    bool constraints_ok_pair(Var x, Value vx, Var y, Value vy, SolverStats* stats = nullptr) const;
+
+    /**
+     * Controlla se l'assegnamento parziale è consistente rispetto la sola Variabile X passata.
+     * @param x Variabile X
+     * @param assignment Assegnamento parziale (o completo)
+     */
+    bool is_consistent_var(Var x, const Assignment& assignment, SolverStats* stats = nullptr) const;
+
+    /**
+     * Controlla se l'assegnamento è consistente considerando tutti i vincoli
+     * @param assignment Assegnamento
+     * @return
+     */
+    bool is_consistent(const Assignment& assignment, SolverStats* stats = nullptr) const;
+
+    /**
+     * @param assignment Assegnamento
+     * @return True se tutte le variabili sono != -1 (CONVENZIONE DI PROGETTO)
+     */
+    bool is_complete(const Assignment& assignment) const;
+
+    /**
+     * Costruisce il grafo primale
+     *
+     * Nodo = Variabile
+     * Arco = Vincolo tra due nodi
+     * @return
+     */
+    std::vector<std::vector<Var>> primal_graph() const; // Grafo primale del CSP
+
+private:
+    std::string name_;
+    Domains domains_;
+    std::vector<std::unique_ptr<BinaryConstraint>> constraints_;
+};
---
+++ src/csp.cpp
@@ -0,0 +1,232 @@
+#include "csp.hpp"
+
+#include <algorithm>
+#include <stdexcept>
+#include <utility>
+
+namespace {
+
+    constexpr UNASSIGNED = -1; // CONVENZIONE DI PROGETTO
+
+    // Utilities varie
+    void check_var(Var x, int nvars) {
+        if (x < 0 || x > nvars) {
+            throw std::out_of_range("Var index out of range!");
+        }
+    }
+
+    void check_assignment_size(const Assignment& assignment, int nvars) {
+        if (static_cast<int>(assignment.size()) != nvars) {
+            throw std::invalid_argument("Invalid assignment size!");
+        }
+    }
+}
+
+BinaryConstraint::BinaryConstraint(Var first, Var second, std::string name): first_(first), second_(second), name_(std::move(name)) {
+    if (first == second) {
+        throw std::invalid_argument("Vars cannot be same!");
+    }
+}
+
+Var BinaryConstraint::first() const {
+    return first_;
+}
+
+Var BinaryConstraint::second() const {
+    return second_;
+}
+
+const std::string &BinaryConstraint::name() const {
+    return name_;
+}
+
+bool BinaryConstraint::involves(Var x) const {
+    return first_ == x || second_ == x;
+}
+
+bool BinaryConstraint::connects(Var x, Var y) const {
+    return (first_ == x && second_ == y)  || (first_ == y && second_ == x);
+}
+
+Var BinaryConstraint::other(Var x) const {
+    if (first_ == x) {
+        return second_;
+    } else if (second_ == x) {
+        return first_;
+    } else {
+        throw std::invalid_argument("Var is not involved in this constraint");
+    }
+}
+
+NotEqualConstraint::NotEqualConstraint(Var first, Var second, std::string name)
+        : BinaryConstraint(first, second, std::move(name)) {
+}
+
+bool NotEqualConstraint::is_satisfied(Value a, Value b) const {
+    return a != b;
+}
+
+LessThanConstraint::LessThanConstraint(Var first, Var second, std::string name)
+        : BinaryConstraint(first, second, std::move(name)) {
+}
+
+bool LessThanConstraint::is_satisfied(Value a, Value b) const {
+    return a < b;
+}
+
+PredicateConstraint::PredicateConstraint(Var first, Var second, std::function<bool(Value, Value)> predicate,
+                                         std::string name) : BinaryConstraint(first, second, std::move(name)),
+                                                             predicate_(std::move(predicate)) {
+    if (!predicate_) {
+        throw std::invalid_argument("PredicateConstraint needs a valid predicate");
+    }
+}
+
+bool PredicateConstraint::is_satisfied(Value a, Value b) const {
+    return predicate_(a, b);
+}
+
+CSP::CSP(std::string name, Domains domains) : name_(name), domains_(domains) {
+    if (domains_.empty()) {
+        throw std::invalid_argument("A CSP must have at least one variable");
+    }
+
+    for (const auto& domain : domains_) {
+        if (domain.empty()) {
+            throw std::invalid_argument("Each variable must have a non-empty domain"); // Altrimenti sarebbe impossibile!
+        }
+    }
+}
+
+const std::string &CSP::name() const {
+    return name_;
+}
+
+const Domains &CSP::domains() const {
+    return domains_;
+}
+
+int CSP::nvars() const {
+    return static_cast<int>(domains_.size());
+}
+
+const std::vector <std::unique_ptr<BinaryConstraint>> &CSP::constraints() const {
+    return constraints_;
+}
+
+Assignment CSP::empty_assignment() const {
+    return Assignment(nvars(), UNASSIGNED);
+}
+
+// Aggiunge un vincolo
+void CSP::add_constraint(std::unique_ptr <BinaryConstraint> constraint) {
+    if (!constraint) {
+        throw std::invalid_argument("Cannot add null constraints");
+    }
+
+    check_var(constraint->first(), nvars());
+    check_var(constraint->second(), nvars());
+
+    constraints_.push_back(std::move(constraint)); // unique ptr non copia, va fatto move
+}
+
+// Funzione cuore
+bool CSP::constraints_ok_pair(Var x, Value vx, Var y, Value vy, SolverStats *stats) const {
+    check_var(x, nvars());
+    check_var(y, nvars());
+
+    for (const auto& constraint : constraints_) {
+        // Skip archi non collegati tra loro
+        if (!constraint->connects(x, y)) continue;
+
+        // Conteggio controlli su vincoli
+        if (stats != nullptr) stats->constraint_checks++;
+
+        bool ok;
+
+        // ATTENZIONE: controllare ordinatamente le variabili!
+        if (constraint->first() == x && constraint->second() == y) ok = constraint->is_satisfied(vx, vy);
+        else if (constraint->second() == y && constraint->second() == x) ok = constraint->is_satisfied(vy, vx);
+
+        if (!ok) return false;
+    }
+
+    return true;
+}
+
+bool CSP::is_consistent_var(Var x, const Assignment &assignment, SolverStats * stats) const {
+    check_var(x, nvars());
+    check_assignment_size(assignment, nvars());
+
+    // Si controlla solo i vincoli valutabili, ossia su variabili assegnate. True non degrada il check
+    if (assignment[x] == UNASSIGNED) return true;
+
+    std::vector<bool> checked(nvars(), false);
+
+    for (const auto& constraint : constraints_) {
+        if (!constraint->involves(x)) continue; // non riguarda X
+
+        Var y = constraint->other(x);
+
+        if (checked[y]) continue;
+
+        checked[y] = true;
+
+        if (assignment[y] == UNASSIGNED) continue;
+
+        // Si controlla la consistenza di arco
+        if (!constraints_ok_pair(x, assignment[x], y, assignment[y], stats)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool CSP::is_consistent(const Assignment &assignment, SolverStats *stats) const {
+    check_assignment_size(assignment, nvars());
+
+    for (const auto& constraint: constraints_) {
+        Var x = constraint->first();
+        Var y = constraint->second();
+
+        if (assignment[x] == UNASSIGNED || assignment[y] == UNASSIGNED) continue;
+
+        if (stats != nullptr) stats->constraint_checks++;
+
+        if (!constraint->is_satisfied(assignment[x], assignment[y])) return false;
+    }
+
+    return true;
+}
+
+bool CSP::is_complete(const Assignment &assignment) const {
+    check_assignment_size(assignment, nvars());
+
+    for (Value value : assignment) {
+        if (value == UNASSIGNED) return false;
+    }
+
+    return true;
+}
+
+std::vector <std::vector<Var>> CSP::primal_graph() const {
+    std::vector<std::vector<Var>> graph(nvars());
+    // Lista di adiacenze, tipo graph[0] = {}, graph[1] = {} ...
+
+    for (const auto& constraint : constraints_) {
+        // Prendo le variabile del vincolo
+        Var x = constraint->first();
+        Var y = constraint->second();
+
+        // Cerco i collegamenti per x e y, controllando per end() evito duplicati
+        auto& gx = graph[x];
+        if (std::find(gx.begin(), gx.end(), y) == gx.end()) gx.push_back(y);
+
+        // Cerco i collegamenti per y e x
+        auto& gy = graph[y];
+        if (std::find(gy.begin(), gy.end(), x) == gx.end()) gy.push_back(x);
+    }
+
+    return graph;
+}
